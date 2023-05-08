4cfd112df658e1d3cc83fd4cfdaee34d292cdecd
Maires Souza <trabalho.maires@gmail.com>
2023-05-08 08:29:52 -0300

    change profile image when toggling

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function toggleMode() {
   const html = document.documentElement
-  // if(html.classList.contains('light')){
-  //   html.classList.remove('light')
-  // } else{
-  //   html.classList.add("light")
-  // }
-
   html.classList.toggle('light')
+  
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if(html.classList.contains('light')){
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/Avatar-light.png")
+    img.setAttribute("alt", "foto de Maires com fone sem fio sorrindo levemente de camisa preta e fundo roxo")
+  } else{
+    // se tiver sem light mode,manter imagem normal
+    img.setAttribute("src", "./assets/Avatar.png")
+    img.setAttribute("alt", "foto de Maires com fone sem fio sorrindo abertamente de camisa branca e fundo roxo")
+  }
+
+
 }
